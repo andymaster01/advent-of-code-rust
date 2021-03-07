42b2e727f955b92ca1ec7cd1fbcb74276453fb2b
Mauro <andymaster01@gmail.com>
2021-03-07 03:38:17 -0500

    refactor day 4

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod general;
 mod y_2015;
 
+use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::process;
 use y_2015::day_1;
 use y_2015::day_2;
 use y_2015::day_3;
@@ -12,29 +15,50 @@ use y_2015::day_4;
 use general::DayResult;
 
 fn main() {
-    let mut res: DayResult;
+    let args: Vec<String> = env::args().collect();
 
-    // res = day_1::exec(&get_contents("./y2015_day1.txt"));
-    // println!("day 1 - floor = {}, position = {}", res.part1, res.part2);
+    if args.len() < 2 {
+        println!("Error: missing folder as 1st param");
+        process::exit(1);
+    }
 
-    // res = day_2::exec(&get_contents("./y2015_day2.txt"));
-    // println!(
-    //     "day 2 - total paper = {}, total ribbon = {}",
-    //     res.part1, res.part2
-    // );
+    if args.len() < 3 {
+        println!("Error: missing year/day to run. Ex: y2015_day1");
+        process::exit(1);
+    }
 
-    // res = day_3::exec(&get_contents("./y2015_day3.txt"));
-    // println!(
-    //     "day 3 - houses = {}, with robo-santa = {}",
-    //     res.part1, res.part2
-    // );
+    if !folder_exists(&args[1]) {
+        println!("Error: can't find folder [{}]", &args[1]);
+        process::exit(1);
+    }
 
-    res = day_4::exec("iwrupvqb");
-    println!("len: {}", res.part1);
+    let folder = &args[1];
+    let day = &args[2];
+    let input = &get_contents(folder, day);
+
+    let res = match day.as_str() {
+        "y2015_day1" => Ok(day_1::exec(input)),
+        "y2015_day2" => Ok(day_2::exec(input)),
+        "y2015_day3" => Ok(day_3::exec(input)),
+        "y2015_day4" => Ok(day_4::exec(input)),
+        &_ => Result::Err(""),
+    };
+
+    match res {
+        Ok(r) => println!("part 1 = {}, part 2 = {}", r.part1, r.part2),
+        Err(_) => println!("Unknown year/day: [{}]", day),
+    };
+
+    println!("bye.")
 }
 
-fn get_contents(path: &str) -> String {
-    let mut file = File::open(Path::new(path)).unwrap();
+fn folder_exists(path: &str) -> bool {
+    fs::metadata(path).is_ok()
+}
+
+fn get_contents(folder: &str, file: &str) -> String {
+    let path = String::from(folder) + "/" + &String::from(file) + ".txt";
+    let mut file = File::open(Path::new(&path)).unwrap();
     let mut s = String::new();
     file.read_to_string(&mut s).unwrap();
 
--- src/y_2015/day_4.rs
+++ src/y_2015/day_4.rs
@@ -1,37 +1,70 @@
 use crate::general::DayResult;
 use md5;
 
-pub fn exec(input: &str) -> DayResult {
-    let top = 100000000;
+fn calculate(input: &str, places: i32, compare: &str, max: i32) -> Option<i32> {
     let mut number = 0;
 
     loop {
         let x = number.to_string();
         let test = String::from(input) + &x;
-
         let digest = md5::compute(&test);
         let parts = format!("{:x}", digest);
+        let mut c = parts.chars();
 
-        let ch = &parts[0..6];
-        // println!("number: {}, test: {}, md5:{}", x, &test, parts);
+        let mut s = String::from("");
 
-        if ch == "000000" {
-            break;
+        for _ in 0..places {
+            let x = c.nth(0).unwrap();
+            s = s + &String::from(x);
+        }
+
+        if s == compare {
+            break Some(number);
         }
 
         number += 1;
 
-        // if number % 1000 == 0 {
-        //     println!("{}", number)
-        // }
-
-        if number == top {
-            break;
+        if number == max {
+            break None;
         };
     }
+}
+
+pub fn exec(input: &str) -> DayResult {
+    let first = calculate(input, 5, "00000", 100000000);
+    let second = calculate(input, 6, "000000", 100000000);
 
     DayResult {
-        part1: number,
-        part2: 0,
+        part1: first.unwrap(),
+        part2: second.unwrap(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_calculate_a() {
+        let r = calculate("a", 2, "32", 10);
+
+        assert_eq!(r.is_some(), true);
+        assert_eq!(r.unwrap(), 5);
+    }
+
+    #[test]
+    fn can_calculate_abcdef() {
+        let r = calculate("abcdef", 5, "00000", 1000000);
+
+        assert_eq!(r.is_some(), true);
+        assert_eq!(r.unwrap(), 609043);
+    }
+
+    #[test]
+    fn can_calculate_pqrstuv() {
+        let r = calculate("pqrstuv", 5, "00000", 10000000);
+
+        assert_eq!(r.is_some(), true);
+        assert_eq!(r.unwrap(), 1048970);
     }
 }
